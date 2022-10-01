04d831ce5267dad8679c0a4da673b4646ce63421
Truth-hash <86872654+Truth-hash@users.noreply.github.com>
2022-10-01 17:29:13 +0000

    tailwind installed

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["./index.html", "./assets/js/**/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", ...defaultTheme.fontFamily.sans],
+        poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
+        heebo: ["Heebo", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        minionYellow: "#FFE347",
+        liberty: "#6457A6",
+        middleBluePurple: "#7D7ABC",
+        lightCoral: "#EF767A",
+        seaGreenCrayola: "#23F0C7",
+        honeydew: "#DFF3E3",
+        khakiWeb: "#C6B89E",
+        rust: "#BC3908",
+        brightYellowCrayola: "#F6AA1C",
+        passionBlue: "#232ED1",
+        seashell: "#FFF4EC",
+        lightCyan: "#E0FBFC",
+      },
+    },
+  },
+  plugins: [],
+};
