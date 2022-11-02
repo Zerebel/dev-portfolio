eee2c7ff130eb0a5e2f48dfa513f6fe8d901d07f
Zerebel <kofitruth11@gmail.com>
2022-11-02 10:42:30 +0000

    header init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-  content: ["./index.html", "./assets/js/**/*.js"],
+  content: ['./index.html', './assets/js/**/*.js'],
   theme: {
     extend: {
       fontFamily: {
-        montserrat: ["Montserrat", ...defaultTheme.fontFamily.sans],
-        poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
-        heebo: ["Heebo", ...defaultTheme.fontFamily.sans],
+        montserrat: ['Montserrat', ...defaultTheme.fontFamily.sans],
+        poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
+        heebo: ['Heebo', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        minionYellow: "#FFE347",
-        liberty: "#6457A6",
-        middleBluePurple: "#7D7ABC",
-        lightCoral: "#EF767A",
-        seaGreenCrayola: "#23F0C7",
-        honeydew: "#DFF3E3",
-        khakiWeb: "#C6B89E",
-        rust: "#BC3908",
-        brightYellowCrayola: "#F6AA1C",
-        passionBlue: "#232ED1",
-        seashell: "#FFF4EC",
-        lightCyan: "#E0FBFC",
+        minionYellow: '#FFE347',
+        liberty: '#6457A6',
+        middleBluePurple: '#7D7ABC',
+        lightCoral: '#EF767A',
+        seaGreenCrayola: '#23F0C7',
+        honeydew: '#DFF3E3',
+        khakiWeb: '#C6B89E',
+        rust: '#BC3908',
+        brightYellowCrayola: '#F6AA1C',
+        passionBlue: '#232ED1',
+        seashell: '#FFF4EC',
+        lightCyan: '#E0FBFC',
+        gray_3: '#828282',
+        gray_1: '#4F4F4F',
       },
     },
   },
